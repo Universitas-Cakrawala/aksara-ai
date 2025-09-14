1672188ba659f8cf9d83c39b592012b4c481d3a0
titan <titanio.yudista@cakrawala.ac.id>
2025-09-14 11:16:41 +0700

    refactor: Remove MongoDB integration and related password update methods from user controller and router

--- aksara-ai-backend/src/user/router.py
+++ aksara-ai-backend/src/user/router.py
@@ -9,11 +9,7 @@ from src.user.schemas import (
     UserCreate,
     UserUpdate,
     UserLogin,
-    Verify2FA,
-    UserGenerateOtpParams,
     PasswordUpdate,
-    PasswordUpdateByUsername,
-    UserSearchParams,
 )
 from src.utils.pagination import PageParams
 
@@ -40,55 +36,13 @@ async def action(
 ):
     return await UserController.login(request, db)
 
-@routerUser.post("/verify_2fa")
-async def action(
-    request: Verify2FA,
-    db: Session = Depends(get_db),
-):
-    return await UserController.verify_2fa(request, db)
 
-@routerUser.post("/generate_otp/{token}")
-async def action(
-    token: str,
-    db: Session = Depends(get_db),
-):
-    return await UserController.generate_otp(token, db)
-
-@routerUser.get("", dependencies=[Depends(JWTBearer())])
-async def action(
-    authorization: str = Header(...),
-    params: UserSearchParams = Depends(),
-    page_params: PageParams = Depends(),
-    db: Session = Depends(get_db),
-):
-    return await UserController.findAll(authorization, params, page_params, db)
-
-
-# TODO: change method POST to GET
-@routerUser.post("/profile", dependencies=[Depends(JWTBearer())])
+@routerUser.get("/profile", dependencies=[Depends(JWTBearer())])
 async def action(
     authorization: str = Header(...),
     db: Session = Depends(get_db),
 ):
     return await UserController.profile(authorization, db)
-
-
-@routerUser.put("/update-password", dependencies=[Depends(JWTBearer())])
-async def action(
-    request: PasswordUpdateByUsername,
-    authorization: str = Header(...),
-    db: Session = Depends(get_db),
-):
-    return await UserController.updatePasswordByUsername(request, authorization, db)
-
-
-@routerUser.get("/{id}", dependencies=[Depends(JWTBearer())])
-async def action(
-    id: str,
-    authorization: str = Header(...),
-    db: Session = Depends(get_db),
-):
-    return await UserController.find(id, authorization, db)
 
 
 @routerUser.delete("/{id}", dependencies=[Depends(JWTBearer())])
@@ -108,15 +62,6 @@ async def action(
     db: Session = Depends(get_db),
 ):
     return await UserController.update(id, request, authorization, db)
-
-
-@routerUser.post("/update-password", dependencies=[Depends(JWTBearer())])
-async def action(
-    request: PasswordUpdate,
-    authorization: str = Header(...),
-    db: Session = Depends(get_db),
-):
-    return await UserController.updatePassword(request, authorization, db)
 
 
 @routerUser.put("/update-password/{id}", dependencies=[Depends(JWTBearer())])
